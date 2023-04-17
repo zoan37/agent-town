30931fc397fb77c124198771d0d951a112ad79c6
zoan37 <104385984+zoan37@users.noreply.github.com>
2023-04-17 17:28:44 -0400

    cloud city large

--- src/start.ts
+++ src/start.ts
@@ -2,94 +2,91 @@ import { Direction, GridEngine } from "grid-engine";
 import * as Phaser from "phaser";
 
 const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
-  active: false,
-  visible: false,
-  key: "Game",
+    active: false,
+    visible: false,
+    key: "Game",
 };
 
 export class GameScene extends Phaser.Scene {
-  private gridEngine!: GridEngine;
+    private gridEngine!: GridEngine;
 
-  constructor() {
-    super(sceneConfig);
-  }
-
-  create() {
-    const cloudCityTilemap = this.make.tilemap({ key: "cloud-city-map" });
-    cloudCityTilemap.addTilesetImage("Cloud City", "tiles");
-    for (let i = 0; i < cloudCityTilemap.layers.length; i++) {
-      const layer = cloudCityTilemap.createLayer(i, "Cloud City", 0, 0);
-      layer.scale = 3;
+    constructor() {
+        super(sceneConfig);
     }
-    const playerSprite = this.add.sprite(0, 0, "player");
-    playerSprite.scale = 1.5;
-    this.cameras.main.startFollow(playerSprite, true);
-    this.cameras.main.setFollowOffset(
-      -playerSprite.width,
-      -playerSprite.height
-    );
 
-    const gridEngineConfig = {
-      characters: [
-        {
-          id: "player",
-          sprite: playerSprite,
-          walkingAnimationMapping: 6,
-          startPosition: { x: 3, y: 3 },
-        },
-      ],
-    };
+    create() {
+        const cloudCityTilemap = this.make.tilemap({ key: "cloud-city-map" });
+        cloudCityTilemap.addTilesetImage("cloud_tileset", "tiles");
+        for (let i = 0; i < cloudCityTilemap.layers.length; i++) {
+            const layer = cloudCityTilemap.createLayer(i, "cloud_tileset", 0, 0);
+            layer.scale = 3;
+        }
+        const playerSprite = this.add.sprite(0, 0, "player");
+        playerSprite.scale = 1.5;
+        this.cameras.main.startFollow(playerSprite, true);
+        this.cameras.main.setFollowOffset(-playerSprite.width, -playerSprite.height);
 
-    this.gridEngine.create(cloudCityTilemap, gridEngineConfig);
-  }
+        const gridEngineConfig = {
+            characters: [
+                {
+                    id: "player",
+                    sprite: playerSprite,
+                    walkingAnimationMapping: 6,
+                    startPosition: { x: 8, y: 8 },
+                },
+            ],
+        };
 
-  public update() {
-    const cursors = this.input.keyboard.createCursorKeys();
-    if (cursors.left.isDown) {
-      this.gridEngine.move("player", Direction.LEFT);
-    } else if (cursors.right.isDown) {
-      this.gridEngine.move("player", Direction.RIGHT);
-    } else if (cursors.up.isDown) {
-      this.gridEngine.move("player", Direction.UP);
-    } else if (cursors.down.isDown) {
-      this.gridEngine.move("player", Direction.DOWN);
+        this.gridEngine.create(cloudCityTilemap, gridEngineConfig);
     }
-  }
 
-  preload() {
-    this.load.image("tiles", "assets/cloud_tileset.png");
-    this.load.tilemapTiledJSON("cloud-city-map", "assets/cloud_city.json");
+    public update() {
+        const cursors = this.input.keyboard.createCursorKeys();
+        if (cursors.left.isDown) {
+            this.gridEngine.move("player", Direction.LEFT);
+        } else if (cursors.right.isDown) {
+            this.gridEngine.move("player", Direction.RIGHT);
+        } else if (cursors.up.isDown) {
+            this.gridEngine.move("player", Direction.UP);
+        } else if (cursors.down.isDown) {
+            this.gridEngine.move("player", Direction.DOWN);
+        }
+    }
 
-    this.load.spritesheet("player", "assets/characters.png", {
-      frameWidth: 52,
-      frameHeight: 72,
-    });
-  }
+    preload() {
+        this.load.image("tiles", "assets/cloud_tileset.png");
+        this.load.tilemapTiledJSON("cloud-city-map", "assets/cloud_city_large.json");
+
+        this.load.spritesheet("player", "assets/characters.png", {
+            frameWidth: 52,
+            frameHeight: 72,
+        });
+    }
 }
 
 const gameConfig: Phaser.Types.Core.GameConfig = {
-  title: "Sample",
-  render: {
-    antialias: false,
-  },
-  type: Phaser.AUTO,
-  scene: GameScene,
-  scale: {
-    width: window.innerWidth,
-    height: window.innerHeight,
-    mode: Phaser.Scale.RESIZE,
-  },
-  plugins: {
-    scene: [
-      {
-        key: "gridEngine",
-        plugin: GridEngine,
-        mapping: "gridEngine",
-      },
-    ],
-  },
-  parent: "game",
-  backgroundColor: "#48C4F8",
+    title: "Sample",
+    render: {
+        antialias: false,
+    },
+    type: Phaser.AUTO,
+    scene: GameScene,
+    scale: {
+        width: window.innerWidth,
+        height: window.innerHeight,
+        mode: Phaser.Scale.RESIZE,
+    },
+    plugins: {
+        scene: [
+            {
+                key: "gridEngine",
+                plugin: GridEngine,
+                mapping: "gridEngine",
+            },
+        ],
+    },
+    parent: "game",
+    backgroundColor: "#48C4F8",
 };
 
 export const game = new Phaser.Game(gameConfig);
